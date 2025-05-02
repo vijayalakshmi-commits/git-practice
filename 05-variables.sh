4e8d9c613c992c757a631107552cfa6f5864afc8
vijayalakshmiy <vijayalakshmiyada@gmail.com>
2025-05-02 19:12:47 +1000

    learning about password and username mention in scripts

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " Please enter your username::"
+
+read -s USERNAME  # takes input into USERNAME variable without the user seeing that
+
+echo "Uername entered is: $USERNAME"
+
+echo " Please enter your password::"
+
+read -s PASSWORD # takes input into the PASSWORD variable without user seeing that
+
+echo "Password entered is:$PASSWORD" # writing this line just to print output, usually this is not right
+
