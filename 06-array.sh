dabb8a862a866f6a0d878f0b0b485892109a98d0
vijayalakshmiy <vijayalakshmiyada@gmail.com>
2025-05-03 18:06:21 +1000

    modified syntax in the script in last line

--- 06-array.sh
+++ 06-array.sh
@@ -6,6 +6,6 @@ echo "First fruit is: ${FRUITS[0]}"
 echo "Second fruit is: ${FRUITS[1]}"
 echo "Third fruit is: ${FRUITS[2]}"
 
-echo "All Fruits are: {$FRUITS[@]}"
+echo "All Fruits are: ${FRUITS[@]}"
 
 
