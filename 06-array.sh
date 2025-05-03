51f4dd40021b6b8124eb49e4ddb228b5a63812bd
vijayalakshmiy <vijayalakshmiyada@gmail.com>
2025-05-03 17:55:05 +1000

    learnt about arrays

---
+++ 06-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("APPLE "KIWI" "ORANGE") #Array
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+echo "All Fruits are: {FRUITS[@]}"
+
+
